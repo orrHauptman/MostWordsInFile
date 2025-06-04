ead3a3a1b497211f17486b11d60f36da23b1a866
Orr Hauptman <or.hau2008@gmail.com>
2025-06-04 14:44:03 +0300

    Count words

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import argv
+from collections import Counter
 
 num : int = int(argv[0])
 
@@ -7,5 +8,5 @@ list_of_words: list[str] = []
 with open("Text.txt" , "r" ) as f :
     list_of_words = f.read().split(" ")
 
-
+appearances: dict [str , int] = Counter(list_of_words)
 
